04dcde43d18b58310f267f5dcf331bdc18d80c4d
Matheus Augusto <matheusalgusto_@hotmail.com>
2025-01-29 02:44:43 -0300

    los factory addition

--- src/losses/factory.py
+++ src/losses/factory.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 
+from losses.triplet_loss import AdaptiveTripletLoss
+
 def get_loss(loss_config):
     loss_name = loss_config["name"]
 
@@ -7,6 +9,8 @@ def get_loss(loss_config):
         loss_fn = nn.BCEWithLogitsLoss()  # For multilabel classification
     elif loss_name == "cross_entropy":
         loss_fn = nn.CrossEntropyLoss()  # For multiclass classification
+    elif loss_name == "adaptative_triplet":    
+        loss_fn = AdaptiveTripletLoss()
     else:
         raise ValueError(f"Loss function {loss_name} is not supported")
 
