3156d0a31a6b0cc2e0eae74ef282dc5a7943bc88
Matheus Augusto <matheusalgusto_@hotmail.com>
2025-05-11 19:34:12 +0000

    Add configuration files for few-shot learning on various cancer datasets
    - Created `uni_fsl_config.yaml` and `resnet_fsl_config.yaml` for skin cancer, ovarian cancer, CRC-VAL-HE-7K, and bracs-resized datasets.
    - Configurations include paths for training and testing data, model settings, loss function, optimizer, training parameters, and data transformations.
    - Implemented logging and output settings for model artifacts and results.
    - Added a script to run the Docker container with GPU support for experiments.

---
+++ run_gpu_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#buid docker image
+# docker build -t retrieval-gpu-experiments .
+
+# run docker image
+docker run -it --rm --runtime=nvidia --gpus all \
+  --shm-size=16g \
+  -v "$(pwd)":/workspaces/master-research-image-retrieval \
+  retrieval-gpu-experiments 
